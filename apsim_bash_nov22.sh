f2b9245bf9b32f674973bdb4f0c4f91a9ea00499
germanmandrini_cpsc <germanm2@illinois.edu>
2020-04-10 17:32:58 -0500

    cluster run

---
+++ apsim_bash_nov22.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=28 ## Specify how many nodes/cores per node
+##PBS -l walltime=00:01:00
+#PBS -q secondary ## Specify which queue to run in
+
+cd /projects/aces
+module load singularity ## Load the singularity runtime to your environment
+# cell_n=$1 #split the cell string for bash work
+cell_n=${cell_n} #for qsub
+
+# echo "$cell_n"
+
+singularity exec /projects/aces/germanm2/apsim_nov16.simg Rscript /projects/aces/germanm2/n_policy/Codes/server_apsim_call.R $cell_n
+
+
+
---
+++ bash_sender_walltime_nov22.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+module load singularity
+
+# singularity exec /projects/aces/germanm2/apsim_nov16.simg Rscript /projects/aces/germanm2/vr_value_v2/Codes/walltime_updater.R
+
+cat /projects/aces/germanm2/n_policy/id_10_walltime.txt | while read i #each line has the cell and the estimated time to be runed
+
+do
+	cell_n="$(cut -d' ' -f1 <<<"$i")" #split the cell string
+	walltime_n="$(cut -d' ' -f2 <<<"$i")" #split the time string
+	echo $cell_n $walltime_n
+	# ./apsim_bash.sh $i
+	qsub -v cell_n=$cell_n -l walltime=00:$walltime_n:00 -N $cell_n ./apsim_bash_nov22.sh #send the cell_n as argument to the script and the walltime in the command line
+	# qsub -v $i -N $i ./apsim_bash.sh
+done
+#qstat -u $USER
+
