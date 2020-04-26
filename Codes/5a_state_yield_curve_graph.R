ebf7db9e2258ab183ea59d41317b3b151223f6ed
germanmandrini-server <germanm2@illinois.edu>
2020-04-26 16:36:53 -0500

    WIP

--- Codes/5a_state_yield_curve_graph.R
+++ Codes/5a_state_yield_curve_graph.R
@@ -16,26 +16,27 @@ grid10_soils_dt5 <- readRDS("./n_policy_box/Data/Grid/grid10_soils_dt5.rds") %>%
 # reg_model_stuff <- readRDS( "./n_policy_box/Data/files_rds/reg_model_stuff.rds")
 # grid10_soils_sf2 <- readRDS('./n_policy_box/Data/Grid/grid10_soils_sf2.rds')
 
-yc_yearly_dt3[,leach_n := leach_1 + leach_2] #update leaching adding corn and soy
-yc_yearly_dt3[, P := Yld * Pc + Yld_soy * Ps - N_fert * Pn] #update profits adding corn and soy
+setnames(yc_yearly_dt3, c('Yld', 'Yld_soy', 'leach_1', 'leach_2'),
+                        c('Y_corn', 'Y_soy', 'L1', 'L2'))
+yc_yearly_dt3[,L := L1 + L2] #update leaching adding corn and soy
+yc_yearly_dt3[,P := Y_corn * Pc + Y_soy * Ps - N_fert * Pn] #update profits adding corn and soy
 #======================================================================================
-# Do two plots with Yld and Leaching for a static N_fert across the state for soy and corn
+# Do two plots with Y_corn and Leaching for a static N_fert across the state for soy and corn
 areas_dt <- grid10_soils_dt5[,.(area_ha = sum(area_ha)), by = .(id_10, mukey)]
 
 state_agg_dt <- merge(yc_yearly_dt3, areas_dt, by = c('id_10', 'mukey'))
 
-state_agg_dt2  <- aggregate_by_area(data_dt = state_agg_dt, variables = c('Yld', 'Yld_soy', 'leach_1','leach_2'), 
+state_agg_dt2  <- aggregate_by_area(data_dt = state_agg_dt, variables = c('Y_corn', 'Y_soy', 'L1','L2'), 
                                     weight = 'area_ha', by_c = c('N_fert'))# %>% .[,-'area_ha']
 baselevel_leach <- 45.6537
-baselevel_leach <- 47
 baselevel_yld <- 11136.46
 baselevel_nfert <- 186.5601
 
 # state_agg_dt2[,leach_prop := round((leach_n / baselevel_leach) - 1,2)*100 ]
 
 plot_1 <- ggplot(data = state_agg_dt2[N_fert < 250]) + 
-  geom_line(aes(x = N_fert, y = Yld, linetype = "Yield")) +
-  geom_line(aes(x = N_fert, y = leach_1*200, linetype = "N Leaching")) +
+  geom_line(aes(x = N_fert, y = Y_corn, linetype = "Yield")) +
+  geom_line(aes(x = N_fert, y = L1*200, linetype = "N Leaching")) +
   #geom_hline(yintercept = baselevel_yld, linetype = 'dashed', color = 'grey', size = 1)+
   geom_vline(xintercept = baselevel_nfert, linetype = 'dashed', color = 'grey', size = 1)+
   labs(y = 'Corn Yield (kg/ha)',
@@ -47,11 +48,7 @@ plot_1 <- ggplot(data = state_agg_dt2[N_fert < 250]) +
   # guides(linetype = guide_legend(order=2),
   #        size = guide_legend(order=1)) +
   theme(legend.title =  element_blank(),
-<<<<<<< HEAD
         legend.position = c(0.87, 0.25),
-=======
-        legend.position = c(0.87, 0.15),
->>>>>>> 7e5c2c9cc5c96bd71011aed9b537c8ffa2b1d302
         legend.text=element_text(size=8),
         panel.grid = element_blank())
 
@@ -61,46 +58,29 @@ ggsave(plot = plot_1, filename = "./n_policy_box/Data/figures/state_response_cur
        units = 'in')
 
 plot_2 <- ggplot(data = state_agg_dt2[N_fert < 250]) + 
-  geom_line(aes(x = N_fert, y = Yld_soy, linetype = "Yield")) +
-<<<<<<< HEAD
-  geom_line(aes(x = N_fert, y = leach_2*120, linetype = "N Leaching")) +
-=======
-  geom_line(aes(x = N_fert, y = leach_2*200, linetype = "N Leaching")) +
->>>>>>> 7e5c2c9cc5c96bd71011aed9b537c8ffa2b1d302
+  geom_line(aes(x = N_fert, y = Y_soy, linetype = "Yield")) +
+  geom_line(aes(x = N_fert, y = L2*120, linetype = "N Leaching")) +
   #geom_hline(yintercept = baselevel_yld, linetype = 'dashed', color = 'grey', size = 1)+
   geom_vline(xintercept = baselevel_nfert, linetype = 'dashed', color = 'grey', size = 1)+
   labs(y = 'Soy Yield (kg/ha)',
        x = 'Corn N rate (kg/ha)',
        colour = "Parameter")+
-<<<<<<< HEAD
   scale_y_continuous(sec.axis = sec_axis(~./200, name = "Soy N leaching (kg/ha)", breaks = seq(0,30,2), labels = seq(0,30,2))) +
-=======
-  scale_y_continuous(sec.axis = sec_axis(~./200, name = "Soy N leaching (kg/ha)", breaks = seq(30,80,5), labels = seq(30,80,5))) +
->>>>>>> 7e5c2c9cc5c96bd71011aed9b537c8ffa2b1d302
   scale_linetype_manual(values = c("dashed", "solid"))+
   theme_bw()+
   # guides(linetype = guide_legend(order=2),
   #        size = guide_legend(order=1)) +
   theme(legend.title =  element_blank(),
-<<<<<<< HEAD
         legend.position = c(0.87, 0.25),
-=======
-        legend.position = c(0.87, 0.15),
->>>>>>> 7e5c2c9cc5c96bd71011aed9b537c8ffa2b1d302
         legend.text=element_text(size=8),
         panel.grid = element_blank())
 
 plot_2
 
-<<<<<<< HEAD
 
-
-ggsave(plot = plot_1, filename = "./n_policy_box/Data/figures/state_response_curve_soy.jpg", width = 5, height = 3,
+ggsave(plot = plot_2, filename = "./n_policy_box/Data/figures/state_response_curve_soy.jpg", width = 5, height = 3,
        units = 'in')
 
 ggsave(plot = grid.arrange(plot_1,plot_2), filename = "./n_policy_box/Data/figures/state_response_curve_both.jpg", width = 5, height = 5,
        units = 'in')
-=======
-ggsave(plot = plot_1, filename = "./n_policy_box/Data/figures/state_response_curve_soy.jpg", width = 5, height = 3,
-       units = 'in')
->>>>>>> 7e5c2c9cc5c96bd71011aed9b537c8ffa2b1d302
+
