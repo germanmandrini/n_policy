bebefddc9dfcb60bb878c438e17d055a0857e3ad
germanmandrini_cpsc <germanm2@illinois.edu>
2020-09-03 08:27:20 -0500

    wip

--- Codes/walltime_updater.R
+++ Codes/walltime_updater.R
@@ -8,37 +8,20 @@ setwd('C:/Users/germanm2/Box Sync/My_Documents') #CPSC
 codes_folder <-'C:/Users/germanm2/Documents'
 
 
-#------------------------
-#Create the walltime
-grid10_soils_dt4 <- readRDS("./n_policy_box/Data/Grid/grid10_soils_dt4.rds")
-grid10_soils_dt4[, z_type := ifelse(id_field %in% c(1,3), 'odd', 'even')]
+#---------------------------------------------------------------------------
 
 
-id_10_walltime_dt <- grid10_soils_dt4[,.N, by = .(id_10, mukey, z_type)] #number of id_10 x mukey x 15z combinations
-id_10_walltime_dt2 <- id_10_walltime_dt[,.N, by = .(id_10)][order(N)] 
-id_10_walltime_dt2[,dur := N*6]
+time_track_walltime_dt <- readRDS("./n_policy_box/Data/files_rds/time_track_walltime_dt.rds")
 
-id_failed <- c(1337, 941, 795)
+id_failed <- c(719)
 
-id_10_walltime_dt2[id_10 %in% id_failed, dur := N*10]
+time_track_walltime_dt[id_10 %in% id_failed, dur := N*10]
 
-id_10_walltime_dt2 <- id_10_walltime_dt2[id_10 %in% id_failed]
+time_track_walltime_dt <- time_track_walltime_dt[id_10 %in% id_failed]
 
+write.table(time_track_walltime_dt[,.(id_10, dur)], paste0('./n_policy_git/time_track_walltime_dt.txt'), row.names = F, col.names = F)
 
-
-write.table(id_10_walltime_dt2[,.(id_10, dur)], paste0(codes_folder,'/n_policy_git/id_10_walltime.txt'), row.names = F, col.names = F)
-
-
-# setwd('C:/Users/germa/Box Sync/My_Documents') #dell
-# codes_folder <-'C:/Users/germa/Documents'
-
-#------------------------
-# Load stations
-# reg_model_stuff <- readRDS("./n_policy_box/Data/files_rds/reg_model_stuff.rds")
-# stations_dt <- reg_model_stuff[['stations']] 
-# rm(reg_model_stuff)
-
-#------------------------
+#---------------------------------------------------------------------------
 
 # files_all <- list.files('./n_policy_box/Data/yc_output_summary/', recursive = T, pattern = '.rds', full.names = F)
 # files_all <- list.files('S:/Bioinformatics Lab/germanm2/n_policy_cluster/yc_output/', recursive = T, pattern = '.rds')
@@ -49,45 +32,10 @@ write.table(id_10_walltime_dt2[,.(id_10, dur)], paste0(codes_folder,'/n_policy_g
 id_10_walltime_dt <- readRDS("./n_policy_box/Data/files_rds/id_10_walltime_dt.rds")
 # id_10_walltime_dt <- readRDS("./n_policy_box/Data/files_rds/time_track_walltime_dt.rds")
 
-#Removed runned
-# id_10_walltime_dt <- id_10_walltime_dt[!id_10 %in% id_10_runned]
 
-
-#Pick only the failed ones
-id_10_walltime_dt[,dur := N *  6]
-id_10_walltime_dt[N >2, dur := N *  5]
-id_10_walltime_dt[N >4, dur := N *  4]
-id_failed <- c(442, 1124,618,953,319,627,924,126,406,825,98,473,1042,260,825, 1116, 
-               1035, 241, 264, 273, 402, 476,  49, 576,  62, 639 ,722, 599, 72,407)
-id_10_walltime_dt <- id_10_walltime_dt[id_10 %in% id_failed, dur := N *  6]
-id_10_walltime_dt <- id_10_walltime_dt[order(dur)]
-# id_failed <- c(1006, 102, 112, 126, 127, 152, 158, 160, 161, 167, 172, 179, 265, 268, 274, 277, 278, 279, 284, 285, 286, 287, 288, 292, 301, 327, 337, 354, 363, 374, 383, 384, 390, 621, 631, 632, 633, 641, 719, 720, 730, 738, 741, 745, 749, 751, 755, 756, 759, 762, 769, 771, 774, 790, 805, 806, 93, 988, 991, 992)
-# id_10_walltime_dt <- id_10_walltime_dt[id_10 %in% id_failed]
-# id_failed2 <- c(1006, 102, 112, 126, 127, 152, 158, 160, 161, 167, 172, 179, 265, 268, 274, 277, 278, 279, 284, 285, 286, 287, 288, 292, 301, 327, 337, 354, 363, 374, 383, 384, 390, 621, 631, 632, 632, 633, 641, 67, 719, 720, 730, 730, 738, 741, 745, 749, 751, 755, 756, 759, 762, 769, 771, 774, 790, 805, 806, 93, 988, 991, 992)
-# setdiff(id_failed2, id_failed)
-
-
-# id_10_walltime_dt <- id_10_walltime_dt[id_10 %in% id_failed]
-#------------------------
-# Filter stations random
-if(FALSE){
-  set.seed(123)
-  stations_dt2 <- stations_dt[,.N, by = .(region, id_10)]
-  id_10_stations <- stations_dt2[,.SD[sample(.N, 5)],by = region]$id_10
-  id_10_walltime_dt <- id_10_walltime_dt[id_10 %in% id_10_stations]
-}
-#------------------------
-# Order stations first all stations
-id_10_stations <- stations_dt[,.N, by = .(region, id_10)]$id_10
-id_10_walltime_dt <- rbind(id_10_walltime_dt[id_10 %in% id_10_stations], 
-                           id_10_walltime_dt[!id_10 %in% id_10_stations])
-#------------------------
-# id_failed_timetrack <- c(935,1095,1184,123,263,29,496,500,842,978,221,1189,1246,406,421,363)
-# id_failed_timetrack <- c(879,291, 1437, )
-# id_10_walltime_dt <- id_10_walltime_dt[id_10 %in% id_failed_timetrack]
-# id_10_walltime_dt[,dur := N *  6]
 
 write.table(id_10_walltime_dt[,.(id_10, dur)], paste0(codes_folder,'/n_policy_git/id_10_walltime.txt'), row.names = F, col.names = F)
+#---------------------------------------------------------------------------
 
 if(FALSE){ #send again files while still running
   
@@ -104,13 +52,12 @@ if(FALSE){ #send again files while still running
 
 
 if(FALSE){ #create the walltime file
+  
   grid10_soils_dt4 <- readRDS("./n_policy_box/Data/Grid/grid10_soils_dt4.rds")
-  id_10_walltime_dt <- grid10_soils_dt4[,.N, by = .(id_10, id_field, mukey)][,-'N']
-  id_10_walltime_dt[id_field == 3 ,id_field := 1 ]
-  id_10_walltime_dt[id_field == 4 ,id_field := 2 ]
-  id_10_walltime_dt <- unique(id_10_walltime_dt )
-  id_10_walltime_dt <- id_10_walltime_dt[,.N, by =id_10][order(N)]
-  id_10_walltime_dt[,dur := N *  2]
+  grid10_soils_dt4[, z_type := ifelse(id_field %in% c(1,3), 'odd', 'even')]
+  id_10_walltime_dt <- grid10_soils_dt4[,.N, by = .(id_10, mukey, z_type)] #number of id_10 x mukey x 15z combinations
+  id_10_walltime_dt2 <- id_10_walltime_dt[,.N, by = .(id_10)][order(N)] 
+  id_10_walltime_dt2[,dur := N*6]
   write.table(id_10_walltime_dt[,.(id_10, dur)], './n_policy/id_10_walltime.txt', row.names = F, col.names = F)
   saveRDS(id_10_walltime_dt, "./n_policy_box/Data/files_rds/id_10_walltime_dt.rds")
 }
@@ -182,3 +129,59 @@ if(FALSE){ #find missing mukeys
   write.table(id_10_walltime_dt2[,.(id_10, dur)], paste0(codes_folder,'/n_policy_git/id_10_walltime.txt'), row.names = F, col.names = F)
   
 }  
+
+if(FALSE){ #Update using timetrack
+  # setwd('C:/Users/germa/Box Sync/My_Documents') #dell
+  setwd('C:/Users/germanm2/Box Sync/My_Documents')#CPSC
+  # setwd("/home/germanm2")
+  # setwd('~')
+  rm(list=ls())
+  
+  source('./Codes_useful/R.libraries.R')
+  
+  files_time <- list.files('S:/Bioinformatics Lab/germanm2/n_policy_cameron/time_track',full.names = T, recursive = T)
+  files_time <- list.files('./n_policy_box/Data/time_track_129/',full.names = T, recursive = T)
+  
+  
+  results_list <- list()
+  for(file_n in files_time){
+    # file_n <- files_daily[2]
+    # print(file_n)
+    results_list[[basename(file_n)]] <- readRDS(file_n)
+  }
+  
+  time_track_dt <- rbindlist(results_list)
+  time_track_dt[mukey_n ==0]
+  
+  time_track_walltime_dt <- time_track_dt[,.(id_10, mukey_n, dur = cell)]
+  ggplot(time_track_walltime_dt) +
+    geom_point(aes(x = mukey_n, y = dur))
+  
+  time_track_walltime_dt[,dur_max := max(dur), by = mukey_n]
+  
+  time_track_walltime_dt[,dur_max := round(dur_max,0)+5]
+  
+  ggplot(time_track_walltime_dt) +
+    geom_point(aes(x = mukey_n, y = dur_max))
+  
+  time_track_walltime_dt[,dur5 := round(dur,0)+5]
+  
+  time_track_walltime_dt[,.(dur = sum(dur),
+                            dur5 = sum(dur5),
+                            dur_max = sum(dur_max))]
+  
+  time_track_walltime_dt <- time_track_walltime_dt[,.(id_10, mukey_n, dur5)][order(dur5) ]
+  
+  setnames(time_track_walltime_dt, 'dur5', 'dur')
+  
+  write.table(time_track_walltime_dt[,.(id_10, dur)], paste0('./n_policy_git/time_track_walltime_dt.txt'), row.names = F, col.names = F)
+  
+  
+  
+  
+  id_10_walltime_dt <- readRDS("./n_policy_box/Data/files_rds/id_10_walltime_dt.rds")
+  comp_dt <- merge(id_10_walltime_dt, time_track_walltime_dt, by = 'id_10')
+  ggplot(comp_dt) +
+    geom_point(aes(x = dur.x, y = dur.y))
+  
+}
