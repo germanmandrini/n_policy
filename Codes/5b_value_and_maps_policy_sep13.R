ba58f8095dd0095a12c5d9745334f9916c38dd24
germanmandrini_cpsc <germanm2@illinois.edu>
2020-10-08 17:22:57 -0500

    wip

--- Codes/5b_value_and_maps_policy_sep13.R
+++ Codes/5b_value_and_maps_policy_sep13.R
@@ -2,10 +2,10 @@ rm(list=ls())
 
 # setwd('C:/Users/germa/Box Sync/My_Documents') #dell
 # codes_folder <-'C:/Users/germa/Documents'#Dell
-# setwd('C:/Users/germanm2/Box Sync/My_Documents')#CPSC
-# codes_folder <-'C:/Users/germanm2/Documents'#CPSC
-setwd('~')#Server
-codes_folder <-'~' #Server
+setwd('C:/Users/germanm2/Box Sync/My_Documents')#CPSC
+codes_folder <-'C:/Users/germanm2/Documents'#CPSC
+# setwd('~')#Server
+# codes_folder <-'~' #Server
 
 
 source('./Codes_useful/R.libraries.R')
@@ -265,17 +265,67 @@ if(FALSE){
 
 
 #--------------------------------------------------------------------------------
-# Nitrogen Cycle
+# N Balance
 
+perfomances_dt2 <- readRDS("./n_policy_box/Data/files_rds/perfomances_dt2.rds") 
+perfomances_dt2[,N_balance := N_fert - Y_corn * 11/1000]
+perfomances_dt2[,L_ton_grain := L/(Y_corn / 1000)]
+balance_dt <- perfomances_dt2[policy == 'ratio_5']
+
+balance_dt[region == 1,region_lab := '1-South']
+balance_dt[region == 2,region_lab := '2-Central']
+balance_dt[region == 3,region_lab := '3-North']
+
+
+ggplot(data = balance_dt)+ geom_density(aes(x = N_balance, colour = NMS))+
+  facet_free(region_lab~.)
+
+ggplot(data = balance_dt[sample(1:nrow(balance_dt), 5000)], aes(x = N_balance, y = L)) + 
+  geom_point()+
+  geom_smooth()+
+  facet_free(region_lab~.)
+
+ggplot(data = balance_dt[sample(1:nrow(balance_dt), 5000)], aes(x = N_balance, y = L_ton_grain)) + 
+  geom_point()+
+  geom_smooth()+
+  facet_free(region_lab~.)
+
+#-------------------------------------------------------------------------
+# YC graph
+# Load Simulated data
 yc_yearly_dt3 <- readRDS("./n_policy_box/Data/files_rds/yc_yearly_dt3.rds")
-cycle_dt <- yc_yearly_dt3[N_fert == 200]
+
+#Add regions and areas
+grid10_soils_dt5 <- readRDS("./n_policy_box/Data/Grid/grid10_soils_dt5.rds")
+areas_dt <- data.table(grid10_soils_dt5) %>% .[,.(area_ha = sum(area_ha)), by = .(id_10, mukey)]
+
+areas_dt <- grid10_soils_dt5[,.(area_ha = sum(area_ha)), by = .(id_10, mukey)]
+
+state_agg_dt <- merge(yc_yearly_dt3[,-c('area_ha')], areas_dt, by = c('id_10', 'mukey'))
+
+state_agg_dt2  <- aggregate_by_area(data_dt = state_agg_dt, variables = c('Y_corn', 'Y_soy', 'L1','L2'), 
+                                    weight = 'area_ha', by_c = c('N_fert', 'region'))# %>% .[,-'area_ha']
+
+state_agg_dt2[,N_balance := N_fert - Y_corn * 11/1000]
+state_agg_dt2[,L := L1 + L2]
+state_agg_dt2[,L_ton_grain := L/(Y_corn / 1000)]
+
+state_agg_dt2[region == 1,region_lab := '1-South']
+state_agg_dt2[region == 2,region_lab := '2-Central']
+state_agg_dt2[region == 3,region_lab := '3-North']
 
 
+ggplot(data = balance_dt)+ geom_density(aes(x = N_balance, colour = NMS))+
+  facet_free(region_lab~.)
 
+ggplot(data = state_agg_dt2) + 
+  geom_point( aes(x = N_balance, y = L))+
+  facet_free(region_lab~.)
 
-
-
-
+ggplot(data = state_agg_dt2) + 
+  geom_point( aes(x = N_balance, y = L_ton_grain))+
+  geom_point( aes(x = N_balance, y =  Y_corn/1000))+
+  facet_free(region_lab~.)
 
 
 
